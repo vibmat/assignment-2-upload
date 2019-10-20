a449b0de34804b8fa5d5188af8d3df07c2dd1302
vibmat <vibhavmathur9@gmail.com>
2019-10-19 23:20:16 -0500

    beta 1

---
+++ mfscrm/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/vibhavmathur/F19-4900-17/Assignments1-2-3/Assign2/foodservice/mfscrm/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
